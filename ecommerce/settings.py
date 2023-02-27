abfeeed70373258fd503357b25edea13aa5a6a3e
EnockOMONDI <enockomondike@gmail.com>
2023-02-27 14:56:35 +0300

    initial

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -39,7 +39,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'shop.apps.ShopConfig',
+    'shop',
     'cart.apps.CartConfig',
     'bootstrap4',
     'pyuploadcare.dj',
